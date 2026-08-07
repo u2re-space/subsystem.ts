fc0ae9f1ca030e271c53c22f77dcb88fac88f1af
L2NE-dev <conway24b@gmail.com>
2026-08-07 10:38:42 +0700

    Shadows in context-menu

--- src/other/document/docx/download.ts
+++ src/other/document/docx/download.ts
@@ -4,14 +4,36 @@ export function safeFilename(name: string): string {
 }
 
 export function downloadBlob(blob: Blob, filename: string): void {
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = filename;
-    a.rel = "noopener";
-    a.style.display = "none";
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
-    setTimeout(() => URL.revokeObjectURL(url), 250);
+    let promised: any = null; // @ts-ignore
+    if (typeof showSaveFilePicker !== "undefined") { // @ts-ignore
+        promised = showSaveFilePicker({
+            suggestedName: filename,
+                types: [{
+                    description: "Document files",
+                    accept: { "application/vnd.openxmlformats-officedocument.wordprocessingml.document": [".docx"] }
+                }] //@ts-ignore
+            })?.then?.(async (fileHandle: FileHandle) => {
+                if (fileHandle) {
+                    const writable = await fileHandle.createWritable();
+                    await writable.write(blob);
+                    await writable.close();
+                } else {
+                    return null;
+                }
+                return fileHandle;
+            })?.catch?.(() => {
+                return null;
+            });
+    } else {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = filename;
+        a.rel = "noopener";
+        a.style.display = "none";
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        setTimeout(() => URL.revokeObjectURL(url), 250);
+    }
 }
